3db4d4a27a35467adf8bb35834ed9f74ccbc63b8
오채린 <chaerin@ochaelin-ui-MacBookAir.local>
2024-08-18 14:42:55 +0900

    init

---
+++ components/common/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/header.module.scss';
+
+interface Props{}
+
+const HeaderComponent = ({}: Props) => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.flexItem}>
+                <Link href="/" className={styles.box}>
+                    <img src="https://lecture-1.vercel.app/inflearn.png" width={110} height={20} alt="인프런 로고" />
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default HeaderComponent;
+
+
+
